352acaa214d999a3b17c6d2d162cd10cfa7e219e
Rajveer-Singh-Git <rajveersinghbareth8@gmail.com>
2025-10-03 19:22:04 +0530

    added local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import {TodoProvider} from "./contexts"
 
@@ -21,6 +21,18 @@ function App() {
   const toggleComplete = () => {
     setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed : !prevTodo.completed}: prevTodo))
   }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   return (
     <TodoProvider value={{todos, addTodo, deleteTodo, updatedTodo, toggleComplete}}>
