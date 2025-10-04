1b69e817ef8e6233e1b5d3bc75f066958df75667
Rajveer-Singh-Git <rajveersinghbareth8@gmail.com>
2025-10-04 14:02:23 +0530

    added functionality to custom components and finalized the webapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import {TodoProvider} from "./contexts"
+import TodoItem from './components/TodoItem'
+import TodoForm from './components/TodoForm'
 
 function App() {
 
@@ -14,12 +16,12 @@ function App() {
     setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
   }
 
-  const deleteTodo = () => {
+  const deleteTodo = (id) => {
     setTodos((prev) => prev.filter((todo) => todo.id !== id))
   }
 
-  const toggleComplete = () => {
-    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed : !prevTodo.completed}: prevTodo))
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed : !prevTodo.completed}: prevTodo))
   }
 
   useEffect(() => {
@@ -40,10 +42,14 @@ function App() {
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */} 
+                        <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
-                        {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                          <div key={todo.id} className='w-full'>
+                            <TodoItem todo={todo}/>
+                          </div>
+                        ))}
                     </div>
                 </div>
             </div>
