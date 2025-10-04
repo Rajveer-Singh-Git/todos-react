165d2e75dd02af30b8a7c2c7fd50fafbd0a1eed1
Rajveer-Singh-Git <rajveersinghbareth8@gmail.com>
2025-10-04 14:10:44 +0530

    enhanced consistency by adding enter press ft

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,40 +1,43 @@
 import React from "react";
-import {useTodo} from "../contexts/TodoContext"
+import { useTodo } from "../contexts/TodoContext"
 import { useState } from "react";
 
 function TodoItem({ todo }) {
-    
+
     const [isTodoEditable, setIsTodoEditable] = useState(false)
     const [todoMsg, setTodoMsg] = useState(todo.todo)
-    const {updatedTodo, deleteTodo, toggleComplete} = useTodo()
+    const { updatedTodo, deleteTodo, toggleComplete } = useTodo()
 
     const editTodo = () => {
-        updatedTodo(todo.id, {...todo, todo : todoMsg})
+        updatedTodo(todo.id, { ...todo, todo: todoMsg })
         setIsTodoEditable(false)
     }
 
     const toggleCompleted = () => {
         toggleComplete(todo.id)
     }
-    
+
 
     return (
         <div
-            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
-                todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
-            }`}
+            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
+                }`}
         >
             <input
-                type="checkbox"
+                type="checkbox"     
                 className="cursor-pointer"
                 checked={todo.completed}
                 onChange={toggleCompleted}
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        toggleCompleted();
+                    }
+                }}
             />
             <input
                 type="text"
-                className={`border outline-none w-full bg-transparent rounded-lg ${
-                    isTodoEditable ? "border-black/10 px-2" : "border-transparent"
-                } ${todo.completed ? "line-through" : ""}`}
+                className={`border outline-none w-full bg-transparent rounded-lg ${isTodoEditable ? "border-black/10 px-2" : "border-transparent"
+                    } ${todo.completed ? "line-through" : ""}`}
                 value={todoMsg}
                 onChange={(e) => setTodoMsg(e.target.value)}
                 readOnly={!isTodoEditable}
@@ -61,7 +64,7 @@ function TodoItem({ todo }) {
                 ❌
             </button>
         </div>
-    ); 
+    );
 }
 
 export default TodoItem;
